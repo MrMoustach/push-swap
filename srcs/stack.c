58ea6ee693e3df87c01ead7ee126b8f532121ee1
Issam Harchi <iharchi@e1r13p8.1337.ma>
2021-03-20 14:46:36 +0100

    fixed stupid error :)

--- srcs/stack.c
+++ srcs/stack.c
@@ -6,7 +6,7 @@
 /*   By: iharchi <iharchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 09:59:49 by iharchi           #+#    #+#             */
-/*   Updated: 2021/03/19 09:39:44 by iharchi          ###   ########.fr       */
+/*   Updated: 2021/03/20 14:46:20 by iharchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,9 @@ void    pop(t_stack stack)
     list = stack.list;
     if (list == NULL)
         return ;
-    while (list->next != NULL)
-    {
-        if (list->next->next == NULL)
-        {
-            tmp = list->next;
-            list->next = NULL;
-            free(tmp);
-            stack.count--;
-            return ;
-        }
-        list = list->next;
-    }
+    tmp = stack.list->next;
+    free(stack.list);
+    stack.list = tmp;
 }
 
 void    push(t_stack stack, int value)
@@ -44,43 +35,32 @@ void    push_to(t_stack stack, t_stack stack2)
 {
     if (ft_stackisempty(stack))
         return ;
-    push(stack2, ft_stacklast(stack.list)->value);
+    push(stack2, stack.list->value);
     pop(stack);
 }
 
 void    swap(t_stack stack)
 {
     int         tmp;
-    t_stacklist *secondtolast;
+    t_stacklist *second;
     int         i;
 
     if (ft_stackisempty(stack))
         return ;
     if (stack.list->next->next == NULL)
         return ;
-    tmp = ft_stacklast(stack.list)->value;
-    secondtolast = stack.list;
-    i = 0;
-    while (i++ < stack.list - 1)
-        secondtolast = secondtolast->value;
-    ft_stacklast(stack.list)->value = secondtolast->value;
-    secondtolast->value = tmp;
+    tmp = stack.list->value;
+    second = stack.list->next;
+    stack.list->value = second->value;
+    second->value = tmp;
 }
 
 void    rotate(t_stack stack)
 {
-    int tmp;
-    int next;
-    t_stacklist *list;
+   t_stacklist *tmp;
 
-    list = stack.list->next;
-    next = stack.list->value;
-    while (list != NULL)
-    {
-        tmp = list->value;
-        list->value = next;
-        next = tmp;
-        list = list->next;
-    }
-    stack.list->value = next;
+   tmp = stack.list;
+   tmp->next = NULL;
+   stack.list = stack.list->next;
+   ft_stacklast(stack.list)->next = tmp;
 }
