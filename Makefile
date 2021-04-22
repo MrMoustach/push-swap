e29fe4ec5453fc69a6f6765e8478d8bedda7a29a
Issam Harchi <iharchi@e1r13p8.1337.ma>
2021-04-22 14:34:30 +0000

    started push_swap

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: iharchi <iharchi@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/03/29 10:24:43 by iharchi           #+#    #+#              #
-#    Updated: 2021/03/30 11:00:30 by iharchi          ###   ########.fr        #
+#    Updated: 2021/04/22 14:12:44 by iharchi          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,12 +15,16 @@ NAME = t
 CHECKER = checker
 PS = push_swap
 
-SRC = srcs/*/*.c
+SRC_UTILS = srcs/stackutils/*.c
+CH_SRC = srcs/checker/*.c
+PS_SRC = srcs/push_swap/*.c
 GNL = gnl/*.c 
 LIBFT = libft/libft.a
 FLAG = -Wall -Werror -Wextra
 ch: 
-		@gcc checker.c -g $(SRC) $(GNL) -D BUFFER_SIZE=32 $(LIBFT) -o $(CHECKER)
+		@gcc checker.c -g $(CH_SRC) $(SRC_UTILS) $(GNL) -D BUFFER_SIZE=32 $(LIBFT) -o $(CHECKER)
+ps:
+		@gcc push_swap.c -g $(PS_SRC) $(SRC_UTILS) $(LIBFT) -o $(PS)
 all: $(NAME)
 $(NAME) : 
 			@gcc $(SRC) $(GNL) $(FLAG)
---
+++ push_swap.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iharchi <iharchi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/20 13:07:24 by iharchi           #+#    #+#             */
+/*   Updated: 2021/04/22 14:11:34 by iharchi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "headers/push_swap.h"
+
+t_stack load_stack(char *av[], int *flag)
+{
+    t_stack a;
+    int     i;
+
+    a.list = NULL;
+    *flag = 0;
+    i = 1;
+    while (av[i])
+    {
+        if (!ft_is_number(av[i]))
+        {
+            *flag = 1;
+            break ;
+        }
+        a = push(a, ft_atoi(av[i]));
+        i++;
+    }
+    return (a);
+}
+
+int main(int ac, char *av[])
+{
+	int	flag;
+	t_stack	a;
+	t_stack	b;
+    
+	if (ac == 1)
+	{
+		write (1, "Error\n", 6);   
+		return (0);
+	}
+	a = load_stack(av, &flag);
+	printstacks(a, b);
+}
